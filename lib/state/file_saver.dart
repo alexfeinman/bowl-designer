51d127a096cd7bba69c8cecea94f9daf8af92d80
Alex Feinman <alexfeinman@gmail.com>
2026-09-04 23:47:48 -0700

    Deploy web build (base href /bowl-designer/)

--- lib/state/file_saver.dart
+++
@@ -1,3 +0,0 @@
-// Platform-neutral entry point for writing a file to disk / downloads.
-// Desktop opens a native save dialog; web triggers a browser download.
-export 'file_saver_io.dart' if (dart.library.js_interop) 'file_saver_web.dart';
