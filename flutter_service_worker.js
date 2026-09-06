113f2f132c3c55ddef562a5ed26b656112025366
Alex Feinman <alexfeinman@gmail.com>
2026-09-06 00:54:39 -0700

    Deploy web build (2026-09-06, base href /bowl-designer/)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4ad5f9f4993fcda16e14d4b6c6959947",
+const RESOURCES = {"flutter_bootstrap.js": "c381fb331fdcbdccb3dc97d88d83e9b4",
 "version.json": "90f7162f113a89f0d695675a1328e303",
 "index.html": "2ead89348ed64a9f05692cd972a99bcb",
 "/": "2ead89348ed64a9f05692cd972a99bcb",
-"main.dart.js": "81e0e89b06826abf0b597d3a25868d4a",
+"main.dart.js": "a14924f695ba10d0353137bc146b18dd",
 "gles_bindings.js": "9c00c4a51e5c48933957966960684267",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
